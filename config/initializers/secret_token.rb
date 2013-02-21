1c7bfbc992a85bdc49edc4cae8773167d043bb97
John Naegle <john.naegle@goodmeasures.com>
2013-02-20 21:43:20 -0600

    Simple Rails 3.1.1 project with a single model with a single field.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scratch::Application.config.secret_token = '731f4f4ac4d617d16d33c86945eaf64cba26ba3cb964b8047f73480a5a509e4d47c1cf75556fc8fad941c68fb0510e6389cf81b3c1e9b400c85371bc2eddf007'
